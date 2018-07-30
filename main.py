ba5f0e2b407ec28c3f0ec1264f6b97f8c2dae191
Renato Teixeira <rena2damas@gmail.com>
2018-07-31 00:08:58 +0200

    wip

---
+++ main.py
@@ -0,0 +1,8 @@
+
+SAPO_ENDPOINT = u'http://services.sapo.pt/EPG/GetChannelByDateInterval?channelSigla={}&startDate={}&endDate={}'
+OMDB_KEY = u'4f225d4b'
+OMDB_BY_ID = u'http://www.omdbapi.com/?i={}'
+OMDB_BY_TITLE = u'http://www.omdbapi.com/?t={}'
+
+if __name__ == '__main__':
+  x = 2
