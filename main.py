f99c125c53c2cba00c0fb7b8d835bca8a3871122
Renato Teixeira <rena2damas@gmail.com>
2018-08-02 01:35:27 +0200

    Performing request

--- main.py
+++ main.py
@@ -4,5 +4,18 @@ OMDB_KEY = u'4f225d4b'
 OMDB_BY_ID = u'http://www.omdbapi.com/?i={}'
 OMDB_BY_TITLE = u'http://www.omdbapi.com/?t={}'
 
+import datetime
+import requests
+
 if __name__ == '__main__':
-  x = 2
+  start = datetime.datetime.now()
+  end =  start + datetime.timedelta(days=1)
+
+  today_start = start.strftime(u'%Y-%m-%d') + u'+00:00:00'
+  today_end = end.strftime(u'%Y-%m-%d') + u'+00:00:00'
+
+  url = SAPO_ENDPOINT.format(u'AXN', today_start, today_end)
+  response = requests.get(SAPO_ENDPOINT)
+
+  print url
+  print response
