d6ad7c978d1a3f7890622daee7b759ea3f8455ac
Renato Teixeira <rena2damas@gmail.com>
2018-09-02 22:49:47 +0200

    Erase entry script

--- erase_data.py
+++
@@ -1,6 +0,0 @@
-from configs.config import CONFIG, db
-
-if __name__ == '__main__':
-    print('Deleted {} movies.'.format(db.movie.delete_many({}).deleted_count))
-    print('Deleted {} schedules.'.format(db.schedule.delete_many({}).deleted_count))
-    print('Deleted {} candidates.'.format(db.candidate.delete_many({}).deleted_count))
